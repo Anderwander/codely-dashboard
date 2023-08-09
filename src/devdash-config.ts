8fe02718c0dc3a1176f7ec2ba9e4e374f5d99a2c
Anderwander <ander1993trabajo@hotmail.com>
2023-08-09 13:02:23 +0200

    añado svgs, creo archivos de infrastructura para abstraer las peticiones etc. A parte de haberlas creado en memoria.

--- src/devdash-config.ts
+++ src/devdash-config.ts
@@ -7,19 +7,19 @@ export interface DevDashConfig {
 }
 
 export const config: DevDashConfig = {
-	github_access_token: process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN as string,
+	github_access_token: import.meta.env.VITE_GITHUB_PERSONAL_ACCESS_TOKEN as string,
 	widgets: [
 		{
 			id: "07340a0f-58e4-4113-bd30-42bc24edc191",
-			repository_url: "https://github.com/Anderwander/Fight",
+			repository_url: "https://github.com/CodelyTV/dotly",
 		},
 		{
 			id: "cbcb3aba-08a9-4e2b-8ffe-addeda0081f9",
-			repository_url: "https://github.com/Anderwander/Andnime",
+			repository_url: "https://github.com/CodelyTV/eslint-plugin-hexagonal-architecture",
 		},
 		{
 			id: "ebd7bb40-45fb-4867-9ba3-08d20257dfc3",
-			repository_url: "https://github.com/Anderwander/hyruleshop",
+			repository_url: "https://github.com/CodelyTV/refactoring-code-smells",
 		},
 	],
 };
--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,3 +1,71 @@
+import React from "react";
+import styles from "./Dashboard.module.scss";
+import { ReactComponent as Lock } from "./lock.svg";
+import { ReactComponent as Unlock } from "./unlock.svg";
+import { ReactComponent as Check } from "./check.svg";
+import { ReactComponent as Error } from "./error.svg";
+import { ReactComponent as Brand } from "./brand.svg";
+import { ReactComponent as Watchers } from "./watchers.svg";
+import { ReactComponent as Forks } from "./repo-forked.svg";
+import { ReactComponent as Start } from "./star.svg";
+import { ReactComponent as PullRequests } from "./git-pull-request.svg";
+import { ReactComponent as IssueOpened } from "./issue-opened.svg";
+import { InMemoryGithubRepositoryRepository } from "../../infrastructure/InMemoryGithubRepositoryrepository";
+
+const isoToReadableDate = (lastUpdate: string): string => {
+	const lastUpdateDate = new Date(lastUpdate);
+	const currentDate = new Date();
+	const diffTime = currentDate.getTime() - lastUpdateDate.getTime();
+	const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
+
+	if (diffDays === 0) {
+		return "today";
+	}
+
+	if (diffDays > 30) {
+		return "more than a month ago";
+	}
+
+	return `${diffDays} days ago`;
+};
+
+const repository = new InMemoryGithubRepositoryRepository();
+
+const repositories = repository.search();
+
 export function Dashboard() {
-	return <div>Dashboard</div>;
+	return (
+		<React.Fragment>
+			<header className={styles.header}>
+				<section className={styles.header__container}>
+					<Brand />
+					<h1 className={styles.app__brand}>DevDash_</h1>
+				</section>
+			</header>
+			<section className={styles.container}>
+				{repositories.map((widget) => (
+					<article className={styles.widget} key={widget.repositoryData.id}>
+						<header className={styles.widget__header}>
+							<a
+								className={styles.widget__title}
+								href={widget.repositoryData.html_url}
+								title={`${widget.repositoryData.organization.login}/${widget.repositoryData.name}`}
+								target="_blank"
+								rel="noreferrer"
+							>
+								{widget.repositoryData.organization.login}/{widget.repositoryData.name}
+							</a>
+							{widget.repositoryData.private ? <Lock /> : <Unlock />}
+						</header>
+						<p>Last update {isoToReadableDate(widget.repositoryData.updated_at)}</p>
+						{widget.ciStatus.workflow_runs.length > 0 && (
+							<div>
+								{widget.ciStatus.workflow_runs[0].status === "completed" ? <Check /> : <Error />}
+							</div>
+						)}
+					</article>
+				))}
+			</section>
+		</React.Fragment>
+	);
 }
