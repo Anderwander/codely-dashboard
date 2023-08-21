81142fc489da0b0706cd0519b20563557c45eb6d
Anderwander <ander1993trabajo@hotmail.com>
2023-08-21 17:25:34 +0200

    Creo el archivo de contexto y el mother para los test. Y se lo paso a factory y eso al componente dashboard. Y Refactorizo el config.

--- src/sections/dashboard/DashboardFactory.tsx
+++ src/sections/dashboard/DashboardFactory.tsx
@@ -4,19 +4,20 @@ import { config } from "../../devdash-config";
 import { GitHubApiGitHubRepositoryRepository } from "../../infrastructure/GithubApiGithubRepositoryRepository";
 import { LocalStorageRepositoryWidgetRepository } from "../../infrastructure/LocalStorageWidgetRepository";
 import { Dashboard } from "./Dashboard";
+import { useRepositoryWidgetContext } from "./repositoryWidget/RepositoryWidgetContextProvider";
 
 const gitHubRepositoryRepository = new GitHubApiGitHubRepositoryRepository(
 	config.github_access_token
 );
 const repositoryWidgetRepository = new LocalStorageRepositoryWidgetRepository();
 
-export class DashboardFactory {
-	static create(): React.ReactElement {
-		return (
-			<Dashboard
-				gitHubRepositoryRepository={gitHubRepositoryRepository}
-				repositoryWidgetRepository={repositoryWidgetRepository}
-			/>
-		);
-	}
+export function DashboardFactory() {
+	const { repositoryWidgets } = useRepositoryWidgetContext();
+	return (
+		<Dashboard
+			gitHubRepositoryRepository={gitHubRepositoryRepository}
+			repositoryWidgetRepository={repositoryWidgetRepository}
+			repositoryWidgets={repositoryWidgets}
+		/>
+	);
 }
